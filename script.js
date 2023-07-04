dceefc3c3f3e6a3299e161436a29bc9a247bf55f
RICHARD TAVERAS <130112620+rich-taveras@users.noreply.github.com>
2023-07-04 09:58:40 -0400

    First Commit
    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Assignment Code
+var luckyNum = document.querySelector("#lucky");
+
+function writeNumbers() {
+    var 
+}
+
+// Add event listener to generate button
+document.addEventListener("click", writeNumbers);
