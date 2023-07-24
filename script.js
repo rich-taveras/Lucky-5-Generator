1667eed29e28945c1da0d7759e79bb22714d4f48
rich taveras <rtaveras@yahoo.com>
2023-07-24 14:28:37 -0400

    added script.js file

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-<<<<<<< HEAD
 // Assignment Code
-var luckyNum = document.querySelector("#lucky");
+const pickNumbers = document.querySelector("#lucky");
 
-function writeNumbers() {
-    var 
+function pickNumbers() {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const evenNumbers = [2, 4, 6, 8, 0];
+  const oddNumbers = [1, 3, 5, 7, 9, 0];
+
+  //function to check if a number is even
+  function isEven(num) {
+    return num % 2 === 0;
+  }
+  //function to check if a number is consecutive with the last picked number
+  function isConsecutive(num) {
+    if (numbers.length === 0) {
+      return false;
+    }
+    return Math.abs(num - numbers[numbers.length - 1]) === 1;
+  }
+  while (numbers.length < 5) {
+    const randomNumber = Math.floor(Math.random() * 36) + 1;
+
+    // Skip if the number is consecutive with the last pick number
+    if (isConsecutive(randomNumber)) {
+      continue;
+    }
+    // Check if number is even or odd and add it to the corresponding array
+    if (isEven(randomNumber)) {
+      evenNumbers.push(randomNumber);
+    } else {
+      oddNumbers.push(randomNumber);
+    }
+    //Ensure that we have at least 2 even and 2 odd numbers
+    if (evenNumbers.length >= 2 && oddNumbers.length >= 2) {
+      //Pick the remaining numbers to get a total of 5 numbers
+      while (numbers.length < 5) {
+        if (evenNumbers.length >= 2) {
+          numbers.push(evenNumbers.pop());
+        } else {
+          numbers.push(oddNumbers.pop());
+        }
+      }
+    }
+  }
+  return numbers;
 }
+const pickdNumbersEl = pickNumbers();
+console.log(pickNumbers);
 
 // Add event listener to generate button
-=======
-// Assignment Code
-var luckyNum = document.querySelector("#lucky");
-
-function writeNumbers() {
-    var 
-}
-
-// Add event listener to generate button
->>>>>>> dceefc3c3f3e6a3299e161436a29bc9a247bf55f
-document.addEventListener("click", writeNumbers);
+// document.addEventListener("click", pickNumbers);
